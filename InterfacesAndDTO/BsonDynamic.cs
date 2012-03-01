a1d8ec76e2dac234a68d64ecd42694f83400c829
Christopher Stolworthy <cstolworthy@gmail.com>
2012-02-29 21:33:30 -0700

    Adding to third party bin

---
+++ InterfacesAndDTO/BsonDynamic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson;
+
+namespace InterfacesAndDTO
+{
+    public class BsonDynamic
+    {
+        public BsonObjectId BsonId { get; set; }
+        public dynamic Fits { get; set; }
+    }
+}
