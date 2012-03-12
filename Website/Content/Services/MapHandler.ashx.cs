70eea97ad91904211908aea044e607e2f154f2a2
Christopher Stolworthy <cstolworthy@gmail.com>
2012-03-11 22:23:30 -0600

    Updating website to add a view for live maps

---
+++ Website/Content/Services/MapHandler.ashx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Content.Services
+{
+    /// <summary>
+    /// Summary description for MapHandler
+    /// </summary>
+    public class MapHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var queryString = context.Request.QueryString;
+
+//            return "/Content/Services/Maphandler.ashx?" + "zoom=" + zoom + "&x=" + coord.x + "&y=" + coord.y + "&client=api&lon=" + va + "&lat=" + ua;
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Website/Controllers/MapsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class MapsController : Controller
+    {
+        //
+        // GET: /Maps/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Overview()
+        {
+            return View();
+        }
+
+    }
+}
